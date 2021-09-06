527f309d6ae93d613fcb7166494468496b298c05
ken chen <ken0965106303@gmail.com>
2021-09-06 21:56:40 +0800

    test

--- matrix_computation.jl
+++
@@ -1,7 +0,0 @@
-function colprod(mat)
-	# input R x C matrix, output 1 X C matrix (each column is column product)
-    # mat = convert(Matrix{Float64}, mat)
-    prodv = ((-1) .^ (sum(mat .< 0, dims=1)) .* exp.(sum(log.(abs.(mat)), dims=1)))
-    return prodv
-end
-
